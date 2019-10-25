8ea32f04ac07f1bbb731a32b8ff3a80e5c304557
Aliia <26248907+aliiae@users.noreply.github.com>
2019-10-25 21:00:25 +0100

    Added favicons

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,10 @@
 import './components/App';
 import '../assets/journal/bootstrap.scss';
 import '../assets/style.scss';
+
+import '../assets/favicon/apple-touch-icon.png';
+import '../assets/favicon/favicon-16x16.png';
+import '../assets/favicon/favicon-32x32.png';
+import '../assets/favicon/foldoverstories.webmanifest';
+import '../assets/favicon/android-chrome-192x192.png';
+import '../assets/favicon/android-chrome-512x512.png';
